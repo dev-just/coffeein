4c82df6e818b5e9c4e606413a9c788c0d0462633
dev-just <152082096+dev-just@users.noreply.github.com>
2026-09-22 13:03:17 +0300

    Добавить шаблон страницы 404

---
+++ views/404.php
@@ -0,0 +1,6 @@
+<div class="text-center py-5">
+    <div class="display-3 fw-bold coffee-number">404</div>
+    <h1 class="h3">Страница не найдена</h1>
+    <a href="/dashboard">Вернуться в личный кабинет</a>
+</div>
+
